28bc63b58a9855d8a59a0174fe9c001547fec9a2
yasser.idrissi <yasser.idrissi@adevinta.com>
2020-01-12 01:05:17 +0100

    update test script

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -2,6 +2,6 @@ describe('My first cypress test', function() {
     it('Login in Avito.ma', function ()  {
         cy.log('Navigate to listing view')
         cy.visit('https://www.avito.ma/')
-
+        cy.contains('abcddd').click();
     })
 })
